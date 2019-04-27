59dbba6c2d3a92f67dbe17e95650d7bff7b30fad
Claire Hardgrove <clairehardgrove@gmail.com>
2019-04-27 17:29:41 +1000

    create should not take id

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from local_config import sql_path
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{sql_path}'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(sql_path)
 db = SQLAlchemy(app)
 
 
@@ -19,8 +19,8 @@ api.add_resource(GetTask, '/api/get/task/<int:task_id>')
 api.add_resource(GetUser, '/api/get/user/<int:user_id>')
 api.add_resource(ChangeTask, '/api/change/task/<int:task_id>')
 api.add_resource(ChangeUser, '/api/change/user/<int:user_id>')
-api.add_resource(CreateTask, '/api/create/task/<int:task_id>')
-api.add_resource(CreateUser, '/api/create/user/<int:user_id>')
+api.add_resource(CreateTask, '/api/create/task')
+api.add_resource(CreateUser, '/api/create/user')
 
 if __name__ == '__main__':
     app.run(debug=True)
