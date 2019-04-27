ae17ac072e618417a08616644ab71b8228c5b218
Claire Hardgrove <clairehardgrove@gmail.com>
2019-04-27 16:45:37 +1000

    move nullable mistkae

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.Text, unique=True, nullable=False)
-    assignee = db.relationship('User', backref=db.backref('tasks', lazy=True), nullable=False)
+    assignee = db.relationship('User', backref=db.backref('tasks', lazy=True))
     assigned_id = db.Column(db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.String(20), nullable=False)
     color = db.Column(db.String(10), unique=False, nullable=False)
