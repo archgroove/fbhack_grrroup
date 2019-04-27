a5873429bd3fac68f68e3dc6f1ea992df946817b
Tom Kunc <tomkunc0@gmail.com>
2019-04-27 16:47:46 +1000

    settings file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-
+from local_config import sql_path
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/tfpk/code/hack/test.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{sql_path}'
 db = SQLAlchemy(app)
 
 
