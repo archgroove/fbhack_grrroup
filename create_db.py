edbb2ae2a422f0d7118aa4da82248e4cff792434
Tom Kunc <tomkunc0@gmail.com>
2019-04-27 16:27:10 +1000

    added database init script

--- create_db.py
+++ create_db.py
@@ -1,7 +1,33 @@
 from app import db
+from models import User, Task
+
 db.create_all()
 
-# Create actual users
+u = User(
+        facebook_details='userid123',
+        email='tomkunc0@gmail.com',
+        name='Tom Kunc'
+    )
 
-# use db.session.add(<object>)
-# then db.sesion.commit()
+u2 = User(
+        facebook_details='userid456',
+        email='Claire@gmail.com',
+        name='Claire'
+    )
+
+for user in [u, u2]:
+    db.session.add(user)
+
+db.session.commit()
+
+t = Task(
+    name='Complete Grrroup',
+    description='Let us get this Hackathon done!',
+    assigned_id=u.id,
+    status='exists',
+    color='0000ff'
+)
+db.session.add(t)
+
+db.session.commit()
+
